947bd34af00a59e03163b22b6a4b655a5f208ccf
markchen <1975016204@qq.com>
2023-04-02 22:18:52 +0800

    添加get请求/hi

--- src/main/java/com/markchen/HelloController.java
+++ src/main/java/com/markchen/HelloController.java
@@ -1,9 +1,17 @@
 package com.markchen;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author markchen
  * @version 1.0
  * @date 2023/4/2 17:13
  */
+@RestController
 public class HelloController {
+    @GetMapping("/hi")
+    public String hello(){
+        return "hello jenkins";
+    }
 }
